e82f18c512313e16cf293825a496c71bae856548
Paul Adams <paul@thecoderszone.com>
2020-10-09 10:59:14 +0100

    Some changes

---
+++ output.py
@@ -0,0 +1,26 @@
+# Copyright Paul Adams <paul@thecoderszone.com>, 2020. All rights reserved.
+# Unauthorized reproduction is prohibited.
+
+from typing import List
+
+# Pretty print a list.
+def human_list(items: List, conj: str = 'and') -> str:
+    # Create a copy of the list so we don't mutate the original.
+    items = items.copy()
+
+    # It is idiomatic in English to bridge the last 2 words with a conjuction.
+    items.insert(len(items) - 1, conj)
+
+    # Lists of 2 typically follow a determiner.
+    if len(items) == 2:
+        determiners = {
+            'and': 'both',
+            'or': 'either'
+        }
+
+        determiner = determiners.get(conj)
+
+        if determiner:
+            items.append(0, determiner)
+
+    return ' '.join(items)
