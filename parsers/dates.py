54628b220c405638f505d7d576a556fe63c21c77
Paul Adams <paul@thecoderszone.com>
2020-10-23 12:14:38 +0100

    Refactored date parsers

--- parsers/dates.py
+++ parsers/dates.py
@@ -2,50 +2,38 @@
 # Unauthorized reproduction is prohibited.
 
 import dateparser
-from typing import List, Type
-from datetime import date as Date
-from argparse import Action as ArgumentParser, ArgumentParser as ArgumentsParser, Namespace as Arguments
+from typing import List
+from firefly import DateRange
+from datetime import date as Date, datetime as DateTime
+from argparse import _StoreAction as StoreAction, ArgumentParser as ArgumentsParser, Namespace as Arguments
+
+# Parse a date string
+def parse_date(date_str: str) -> Date:
+    time: DateTime = dateparser.parse(date_str)
+
+    if not time:
+        raise ValueError('Unable to parse date `%s`. Perhaps try rephrasing it?' % date_str)
+
+    return time.date()
 
 # Parses a date string
-class DateParser(ArgumentParser):
-    # Invokes the parser function
+class DateParser(StoreAction):
+    # Invoke the parser function
     def __call__(self, parser: ArgumentsParser, args: Arguments, date_str: str, option_string: str = None):
-        setattr(args, self.dest, self._parse_date(date_str))
-
-    # Parse a date string
-    def _parse_date(self, date_str: str) -> Date:
-        time = dateparser.parse(date_str)
-
-        if not time:
-            raise ValueError('Unable to parse date `%s`. Perhaps try rephrasing it?' % date_str)
-
-        return time
+        super().__call__(parser, args, parse_date(date_str), option_string)
 
 # Parses multiple date strings
-class DatePeriodParser(DateParser):
-    # Invokes the parser function
+class DatePeriodParser(StoreAction):
+    # Invoke the parser function
     def __call__(self, parser: ArgumentsParser, args: Arguments, dates: List[str], option_string: str = None):
         for i, date_str in enumerate(dates):
-            dates[i] = self._parse_date(date_str)
+            dates[i] = parse_date(date_str)
 
-        from_date = dates[0]
+        from_date: Date = dates[0]
 
         try:
-            until_date = dates[1]
+            until_date: Date = dates[1]
         except IndexError:
             until_date = None
 
-        setattr(args, self.dest, DateRange(from_date, until_date))
-
-# Encapsulates 2 dates
-class DateRange():
-    # Create an instance
-    def __init__(self, from_date: Date, until_date: Date = None):
-        self.from_date: Date = from_date
-        self._until_date: Date = until_date
-
-    # Get the until date
-    @property
-    def until_date(self):
-        # Default to single date range
-        return self._until_date or self.from_date
+        super().__call__(parser, args, DateRange(from_date, until_date), option_string)
