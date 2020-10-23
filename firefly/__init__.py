325b4ee7c441ca8e58f15caf30154b45f1cf4172
Paul Adams <paul@thecoderszone.com>
2020-10-23 12:13:36 +0100

    Firefly module

--- firefly/__init__.py
+++ firefly/__init__.py
@@ -2,5 +2,8 @@
 # Unauthorized reproduction is prohibited.
 
 from .client import Client
+from .filters import Sort, DateRange
+from .events import TaskEvent, MarkAsDoneEvent, MarkAsUndoneEvent
 from .resources import User, Teacher, Lesson, Addressee, Class, Student, Task
-from .enums import TaskCompletionStatus, TaskReadStatus, TaskMarkingStatus, SortDirection, SortColumn, TaskSortColumn, FilterEnum, TimetablePeriod, TaskOwner, TaskEvent
+from .enums import (Enum, TaskCompletionStatus, TaskReadStatus, TaskMarkingStatus, SortDirection,
+                    SortColumn, TaskSortColumn, FilterEnum, TimetablePeriod, TaskOwner, TaskEventEnum)
