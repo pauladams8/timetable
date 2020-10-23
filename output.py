1f884cde2e2b122c849d35003956b103682db124
Paul Adams <paul@thecoderszone.com>
2020-10-23 12:07:50 +0100

    Output

--- output.py
+++ output.py
@@ -1,26 +1,27 @@
 # Copyright Paul Adams, 2020. All rights reserved.
 # Unauthorized reproduction is prohibited.
 
-from typing import List
+from typing import List, Dict
+
+# Conj - determiner lookup
+_DETERMINERS: dict = {
+    'and': 'both',
+    'or': 'either'
+}
 
 # Pretty print a list
-def human_list(items: List, conj: str = 'and') -> str:
+def human_list(items: List[str], conj: str = 'and', determiners: bool = False) -> str:
     # Create a copy of the list so we don't mutate the original
-    items = items.copy()
+    items: List = items.copy()
 
     # It is idiomatic in English to bridge the last 2 words with a conjuction
-    items.insert(len(items) - 1, conj)
+    items.append(' '.join([items.pop(), conj, items.pop()]))
 
-    # Lists of 2 typically follow a determiner
-    if len(items) == 2:
-        determiners = {
-            'and': 'both',
-            'or': 'either'
-        }
-
-        determiner = determiners.get(conj)
+    # Lists of 2 can follow a determiner
+    if determiners and len(items) == 2:
+        determiner: str = _DETERMINERS.get(conj)
 
         if determiner:
-            items.append(0, determiner)
+            items.insert(0, determiner + ' ' + items.pop(0))
 
-    return ' '.join(items)
+    return ', '.join(items)
