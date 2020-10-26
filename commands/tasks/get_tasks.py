8d53649dc5f9770ec3591317410e7dc19d3f7ddc
Paul Adams <paul@thecoderszone.com>
2020-10-26 02:20:39 +0000

    Make "to do" default completion status

--- commands/tasks/get_tasks.py
+++ commands/tasks/get_tasks.py
@@ -29,7 +29,7 @@ class GetTasks(Command):
     # Register the command arguments
     def register_arguments(self):
         completion_status_group = self.parser.add_mutually_exclusive_group()
-        completion_status_group.add_argument('--todo', dest='completion_status', action='store_const', const=TaskCompletionStatus.TO_DO, help='Tasks that are yet to be completed')
+        completion_status_group.add_argument('--all', dest='completion_status', action='store_const', const=TaskCompletionStatus.ALL, help='Tasks that are yet to be completed')
         completion_status_group.add_argument('--done', dest='completion_status', action='store_const', const=TaskCompletionStatus.DONE, help='Tasks that are done')
         read_status_group = self.parser.add_mutually_exclusive_group()
         read_status_group.add_argument('--read', dest='read_status', action='store_const', const=TaskReadStatus.READ, help='Tasks that have been read')
@@ -37,11 +37,11 @@ class GetTasks(Command):
         marking_status_group = self.parser.add_mutually_exclusive_group()
         marking_status_group.add_argument('--marked', dest='marking_status', action='store_const', const=TaskMarkingStatus.MARKED, help='Tasks that have been marked')
         marking_status_group.add_argument('--unmarked', dest='marking_status', action='store_const', const=TaskMarkingStatus.UNMARKED, help='Tasks that have not been marked')
-        self.parser.add_argument('--set', action=DatePeriodParser, nargs='*', metavar=('FROM', '[UNTIL]'), help='Tasks that were set no earlier than FROM, but no later than UNTIL. UNTIL defaults to FROM, so for tasks set yesterday, for example, use --set yesterday. ' + self.INTELLEGENT_DATE_HINT)
-        self.parser.add_argument('--due', action=DatePeriodParser, nargs='*', metavar=('FROM', '[UNTIL]'), help='Tasks that are due no earlier than FROM, but no later than UNTIL. UNTIL defaults to FROM, so for tasks due tomorrow, for example, use --due tomorrow. ' + self.INTELLEGENT_DATE_HINT)
+        self.parser.add_argument('--set', action=DatePeriodParser, nargs='+', metavar=('FROM', '[UNTIL]'), help='Tasks that were set no earlier than FROM, but no later than UNTIL. UNTIL defaults to FROM, so for tasks set yesterday, for example, use --set yesterday. ' + self.INTELLEGENT_DATE_HINT)
+        self.parser.add_argument('--due', action=DatePeriodParser, nargs='+', metavar=('FROM', '[UNTIL]'), help='Tasks that are due no earlier than FROM, but no later than UNTIL. UNTIL defaults to FROM, so for tasks due tomorrow, for example, use --due tomorrow. ' + self.INTELLEGENT_DATE_HINT)
         self.parser.add_argument('--set-by', nargs='*', metavar='SETTER', help='Space separated list of task setter GUIDs. You can get a list of your teachers and their GUIDs using `%(prog)s teachers`.')
         self.parser.add_argument('--set-to', nargs='*', metavar='ADDRESEE', help='Space separated list of task addressee GUIDs. You can get a list of your classes and their GUIDs using `%(prog)s classes`.')
-        self.parser.add_argument('--sort', action=TaskSortParser, nargs=2, metavar=('COLUMN', '[DIRECTION]'), default=Sort(TaskSortColumn.DUE_DATE), help='Order by which to sort the tasks. Supply a snake_case column (either set_date or due_date; defaults to due_date) and optionally a direction (either asc or desc; defaults to desc).')
+        self.parser.add_argument('--sort', action=TaskSortParser, nargs='+', metavar=('COLUMN', '[DIRECTION]'), default=Sort(TaskSortColumn.DUE_DATE), help='Order by which to sort the tasks. Supply a snake_case column (either set_date or due_date; defaults to due_date) and optionally a direction (either asc or desc; defaults to desc).')
         self.parser.add_argument('--offset', type=int, default=0, help='Offset from which to retrieve tasks (defaults to 0). Must be an integer.')
         self.parser.add_argument('--limit', type=int, default=10, help='Limit to retrieve tasks to (defaults to 10). Must be an integer.')
 
@@ -49,7 +49,7 @@ class GetTasks(Command):
     def __call__(self, args: Arguments):
         tasks, total_count = self.print_client_state(
             lambda: self.firefly_client.get_tasks(
-                completion_status=args.completion_status or TaskCompletionStatus.ALL,
+                completion_status=args.completion_status or TaskCompletionStatus.TO_DO,
                 read_status=args.read_status or TaskReadStatus.ALL,
                 marking_status=args.marking_status or TaskMarkingStatus.ALL,
                 sort=args.sort,
