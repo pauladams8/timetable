8168a6ae8e9ce566c56c25f498d0b2241bc3aae4
Paul Adams <paul@thecoderszone.com>
2020-10-26 02:34:09 +0000

    Make "to do" default task completion status

--- firefly/filters.py
+++ firefly/filters.py
@@ -2,7 +2,7 @@
 # Unauthorized reproduction is prohibited.
 
 from datetime import date as Date
-from .enums import SortColumn, SortDirection
+from .enums import SortColumn, SortDirection, TaskCompletionStatus
 
 # Encapsulates the sort column and direction
 class Sort():
@@ -12,10 +12,16 @@ class Sort():
         self._direction: SortDirection = direction
 
     # Get the direction
-    @property
-    def direction(self):
-        # Default to descending as most users won't want to trawl through old homework archives
-        return self._direction or SortDirection.DESCENDING
+    def direction(self, completion_status: TaskCompletionStatus = TaskCompletionStatus.ALL):
+        if self._direction:
+            return self._direction
+
+        # Default to ascending if the user wants only uncompleted tasks, so they know what to prioritise
+        if completion_status == TaskCompletionStatus.TO_DO:
+            return SortDirection.ASCENDING
+
+        # Else, default to descending as most users won't want to trawl through old homework archives
+        return SortDirection.DESCENDING
 
 # Encapsulates 2 dates
 class DateRange():
@@ -26,6 +32,6 @@ class DateRange():
 
     # Get the until date
     @property
-    def until_date(self):
+    def until_date(self) -> Date:
         # Default to single date range
         return self._until_date or self.from_date
