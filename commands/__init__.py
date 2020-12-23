e5f7891b601d307c4b4598fc1690a6bbb4c25ff7
Paul Adams <paul@thecoderszone.com>
2020-12-23 04:48:22 +0000

    Moved commands

--- commands/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright Paul Adams, 2020. All rights reserved.
-# Unauthorized reproduction is prohibited.
-
-from .command import Command
--- commands/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright Paul Adams, 2020. All rights reserved.
-# Unauthorized reproduction is prohibited.
-
-from .get_tasks import GetTasks
-from .undo_task import UndoTask
-from .complete_task import CompleteTask
