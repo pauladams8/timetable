908ce7cdbd848465940812ab4e61a5a84fec8cc9
Paul Adams <paul@thecoderszone.com>
2020-12-23 04:48:36 +0000

    Moved into module

--- parsers/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright Paul Adams, 2020. All rights reserved.
-# Unauthorized reproduction is prohibited.
-
-from .sort import SortParser, TaskSortParser, Sort
-from .dates import DateParser, DatePeriodParser, DateRange
