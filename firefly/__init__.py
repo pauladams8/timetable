509ec8ea11ffb092db5c16e2a003812ebca24221
Paul Adams <paul@thecoderszone.com>
2020-12-23 04:51:51 +0000

    Refactor names

--- firefly/__init__.py
+++ firefly/__init__.py
@@ -2,8 +2,9 @@
 # Unauthorized reproduction is prohibited.
 
 from .client import Client
-from .filters import Sort, DateRange
+from .filters import Sort, TaskSort, DatePeriod
 from .events import TaskEvent, MarkAsDoneEvent, MarkAsUndoneEvent
 from .resources import User, Teacher, Lesson, Addressee, Class, Student, Task
+from .errors import FireflyError, AuthenticationError, ConfigError, InputError
 from .enums import (Enum, TaskCompletionStatus, TaskReadStatus, TaskMarkingStatus, SortDirection,
                     SortColumn, TaskSortColumn, FilterEnum, TimetablePeriod, TaskOwner, TaskEventEnum)
